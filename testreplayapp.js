6f5250b596ca2be91cbe819c26bf82cbb16a8bcc
Daniel <danieljanderson0616@gmail,com>
2018-03-29 19:26:50 +0000

    setup callback function

--- testreplayapp.js
+++ testreplayapp.js
@@ -1,8 +1,9 @@
 function init() {
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1dtMUq0qZvXBkplP2w-zleMOYxjyY6M7YcDwlz8uZS1k/edit?ts=5ab98793#gid=0',
-                   callback: function(data, tabletop) { 
-                       console.log(data)
-                   },
+                   callback: test ,
                    simpleSheet: true } )
+}
+function test(data){
+  console.log('this is a test function',data)
 }
 window.addEventListener('DOMContentLoaded', init)
