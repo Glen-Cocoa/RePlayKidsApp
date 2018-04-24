4475875fc4dfb7d7282676c9a1c42d00adbdc524
Eli <eli.steiger@gmail.com>
2018-04-24 01:58:44 +0000

    branch for joseph and demo

--- table/objectconstructor.js
+++ table/objectconstructor.js
@@ -1,19 +1,27 @@
 let data2 = new parseFile.ConfigObj(function(data){
-    let good_data = data.filteredData()
+    let good_data = data.filteredData();
     $('#my-final-table').dynatable({
       dataset: {
-      records: good_data
-    }
-  });
-  for(let i=0;i<good_data;i++){
-    console.log(good_data[i])
-  }
+        records: good_data
+      }
+    });
+    $('#my-final-table tr').on('click', function(e) {
+      var uid = $(this).find('td:first').text();
+      var obj;
+      for(var i = 0; i < good_data.length; i++) {
+        if(good_data[i]['uniqueId'] == uid) {
+          obj = good_data[i];
+          break;
+        }
+      }
+      var $modal = $('.detail-page');
+      
+      // Update modal content
+      $modal.find('h2').text(obj.name);
+      
+      $('.detail-page').modal('show');
+    });
+  
+    console.log(data.recordArray[0].Photo)
 })
 
-$(function(){
-  setTimeout(function(){
-    $('td').on('hover',function(e){
-      console.log('done')
-    })
-  }, 2000)
-})
