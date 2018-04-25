1024e39618173030b2d94f420cebd6326a468739
DavidHargravesJr <dmhjr82@yahoo.com>
2018-04-25 01:55:22 +0000

    Changed table headers

--- testreplayapp.js
+++ testreplayapp.js
@@ -22,16 +22,17 @@ var parseFile =(function(){
       for(var i = 0; i < _this.recordArray.length; i++) {
         var originalObj = _this.recordArray[i];
         arr.push({
+          "toyName": originalObj["Toy name"],
           "name": originalObj["Toy name"],
           "type": originalObj["Toy Type"],
           "instructions": originalObj["Instructions"],
-          "sensory": originalObj["Sensory output type"],
+          "sensoryType": originalObj["Sensory output type"],
           "manufacturer": originalObj["Manufacturer"],
           "photo": originalObj["Photo"],
-          "instruction format": originalObj["Instruction Format"],
+          "instructionType": originalObj["Instruction Format"],
           "toyType": originalObj["Toy Type"],
           "switchConnectionLocation": originalObj["Switch connection location"],
-          "adaptingGrouporPerson": originalObj["Adapting Group or Person"],
+          "adaptingGroup": originalObj["Adapting Group or Person"],
           "keywords": originalObj["Keywords"],
           "summary": originalObj["Summary"],
           "difficultyLevel": originalObj["Difficulty Level"],
