f7f0239ec008d4adda816ae793084eec087c2ae1
JParnellCox <jcox74@live.com>
2018-04-25 20:37:58 -0400

    Added changes to index.html that allows update for adapting group diffculty level to be dyn updated

--- table/objectconstructor.js
+++ table/objectconstructor.js
@@ -5,7 +5,7 @@ let data2 = new parseFile.ConfigObj(function(data){
         records: good_data
       }
     });
-    $('#my-final-table').on('click','tr', function(e) {
+    $('#my-final-table').on('click','tr', function(e) {         
       var uid = $(this).find('td:first').text();
       var obj;
       for(var i = 0; i < good_data.length; i++) {
@@ -14,19 +14,22 @@ let data2 = new parseFile.ConfigObj(function(data){
           break;
         }
       }
+        
+        
       var $modal = $('.detail-page').modal('show');
       
       // Update modal content
-      $modal.find('h2').text(obj.name);
+      $modal.find('h2').text(obj.name);      
       $modal.find('#manufacturer').text(obj.manufacturer);
-      $modal.find('#sensoryOutput').text(obj.sensory); 
+      $modal.find('#sensoryOutput').text(obj.sensoryType); 
       $modal.find('#toyType').text(obj.toyType);
-      $modal.find('#link').text(obj.instructions);
+      $modal.find('#link').text(obj.instructions);        
       $modal.find('#difficultyLevel').text(obj.difficultyLevel);
       $modal.find('#keywords').text(obj.keywords);    
-      $modal.find('#Adapting Group').text(obj.adaptingGrouporPerson);    
+      $modal.find('#adaptingGroup').text(obj.adaptingGroup);    
       
       $('.detail-page').modal('show');
+    
     });
   
     console.log(data.recordArray[0].Photo)
